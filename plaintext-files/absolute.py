3bab6127bcb73fedf7186f602e07eee8ff725e73
Vong Hoang Vinh <vonghoangvinh1989@gmail.com>
2026-07-28 19:10:53 -0600

    update code of section 4 understanding paths

---
+++ plaintext-files/absolute.py
@@ -0,0 +1,4 @@
+path = 'E:\\LearningCourses\\ZeroToMastery\\ztm-the-python-automation-bootcamp\\plaintext-files\\absolute_v_relative\\dad_jokes.txt'
+
+with open(path, 'r') as file:
+    print(file.read())
